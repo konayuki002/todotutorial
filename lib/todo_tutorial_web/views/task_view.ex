15b666db7dd7aa8bdb147d6005add8f4258c2bda
konayuki002 <konayuki0455@gmail.com>
2020-02-25 13:27:38 +0900

    add doc example case

--- lib/todo_tutorial_web/views/task_view.ex
+++ lib/todo_tutorial_web/views/task_view.ex
@@ -1,13 +1,26 @@
 defmodule TodoTutorialWeb.TaskView do
   use TodoTutorialWeb, :view
+  @moduledoc false
 
   @doc """
   Returns true if the user_id is selected in params.
+  The answer is depends on the "user_id" in @conn[params]
 
   ## Examples
 
-      iex> is_selected(@conn, "2")
+      iex> @conn[params]["user_id"] = "nil"
+      ...> is_selected(@conn, "nil")
       true
+
+      iex> @conn[params]["user_id"] = "2"
+      ...> is_selected(@conn, 2)
+      true
+
+      iex> is_selected(@conn, "nil")
+      true
+
+      iex> is_selected(@conn, 2)
+      false
 
   """
   @spec is_selected(%Plug.Conn{params: %{required(charlist) => charlist}}, charlist) :: boolean
