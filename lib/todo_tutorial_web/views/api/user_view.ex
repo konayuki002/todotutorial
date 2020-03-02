583dd75e219de12c768a62f16d994ce1c7ad9203
konayuki002 <konayuki0455@gmail.com>
2020-03-02 11:34:40 +0900

    fix json struct

--- lib/todo_tutorial_web/views/api/user_view.ex
+++ lib/todo_tutorial_web/views/api/user_view.ex
@@ -11,6 +11,9 @@ defmodule TodoTutorialWeb.Api.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id}
+    %{
+      id: user.id,
+      name: user.name
+    }
   end
 end
