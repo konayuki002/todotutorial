f2919dee0116ed16ffcc4ec9dc3ada7997f7bd7e
konayuki002 <konayuki0455@gmail.com>
2020-03-08 18:17:50 +0900

    add doc

--- lib/todo_tutorial_web/views/api/task_view.ex
+++ lib/todo_tutorial_web/views/api/task_view.ex
@@ -6,6 +6,15 @@ defmodule TodoTutorialWeb.Api.TaskView do
     %{data: render_many(tasks, TaskView, "task.json")}
   end
 
+  @doc """
+    return urgent and expired tasks
+
+    ## Examples
+
+      iex> render("urgent.json", %{urgent_tasks: [%Task{}], expired_tasks: [%Task{}]})
+      %{data: %{urgent_tasks: [%Tasks{}], expired_tasks:[%Tasks{}]}}
+
+  """
   def render("urgent.json", %{urgent_tasks: urgent_tasks, expired_tasks: expired_tasks}) do
     %{data: %{urgent_tasks: render_many(urgent_tasks, TaskView, "task.json"), expired_tasks: render_many(expired_tasks, TaskView, "task.json")}}
   end
